34708ba5040db7ba1b6aea373b50e570474b488d
Clerson Clervius Verdieu Vincent <soncler05@gmail.com>
2020-03-07 10:49:06 -0600

    End of chapter 7

--- src/async.js
+++ src/async.js
@@ -1,78 +1,121 @@
 import * as d3 from "d3";
-import { Subject, interval, animationFrameScheduler } from "rxjs";
+import { Subject, interval, animationFrameScheduler, BehaviorSubject, ReplaySubject } from "rxjs";
 import {take } from "rxjs/operators";
 import { animationFrame } from "rxjs/internal/scheduler/animationFrame";
-
-const waveData = new Subject();
-
-
-const audioCtx = new AudioContext();
-const audioElement = document.getElementById('audioElement');
-
-//Canvas setup
-const canvas = document.querySelector('canvas');
-const canvasCtx = canvas.getContext('2d');
-const width = canvas.width;
-const height = canvas.height;
+import {add} from './helpers'
 
 
 
-//Web Audio API setup
-const audioSource = audioCtx.createMediaElementSource(audioElement);
-const analyser = audioCtx.createAnalyser();
-
-analyser.fftSize = 1024;
-const bufferLength = analyser.frequencyBinCount;
-
-audioCtx.resume().then(() =>{
-    console.log('Playback resumed successfully!!!');
-});
-
-audioSource.connect(analyser);
-audioSource.connect(audioCtx.destination);
-
-const dataArray = new Uint8Array(bufferLength);
-analyser.getByteTimeDomainData(dataArray);
 
 
-//D3 Setup
-const x = d3.scaleLinear().
-domain([0, analyser.frequencyBinCount]).
-range([0, width]);
-
-const y = d3.scaleLinear().
-domain([-175, 175]).
-range([height, 175]);
-
-const line = d3.line().
-x(function(d, i) {return x(i);}).
-y(function(d) {return y(d);}).
-context(canvasCtx);
 
 
-function renderFullLine(d){
-    canvasCtx.strokeStyle = "rgb(255, 0,  100)";
-    canvasCtx.beginPath();
-    line(d);
-    canvasCtx.stroke();
-} 
 
-function renderLoop(){
-    analyser.getByteTimeDomainData(dataArray);
-    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-    waveData.next(dataArray);
-}
 
-waveData.subscribe(
-    d => {
-        return renderFullLine(d);
-    }
+
+
+
+
+const nums = new ReplaySubject(2);
+
+interval(1000).subscribe(
+    x => nums.next(x)
+);
+
+nums.
+pipe(take(5)).
+subscribe(add.li);
+
+setTimeout(
+    () => nums.pipe(take(10)).subscribe(x=>add.li(`second: ${x}`)), 7000
 )
 
 
-const loop = interval(0, animationFrameScheduler).pipe(
-    take(2000)
-).subscribe(renderLoop);
+// const nums = new BehaviorSubject(2288);
+
+// interval().subscribe(
+//     x => nums.next(x)
+// );
+
+// nums.
+// pipe(take(5)).
+// subscribe(add.li);
+
+
+
+
+
+// // ----------------------------
+// const waveData = new Subject();
+
+
+// const audioCtx = new AudioContext();
+// const audioElement = document.getElementById('audioElement');
+
+// //Canvas setup
+// const canvas = document.querySelector('canvas');
+// const canvasCtx = canvas.getContext('2d');
+// const width = canvas.width;
+// const height = canvas.height;
+
+
+
+// //Web Audio API setup
+// const audioSource = audioCtx.createMediaElementSource(audioElement);
+// const analyser = audioCtx.createAnalyser();
+
+// analyser.fftSize = 1024*2;
+// const bufferLength = analyser.frequencyBinCount;
+
+// audioCtx.resume().then(() =>{
+//     console.log('Playback resumed successfully!!!');
+// });
+
+// audioSource.connect(analyser);
+// audioSource.connect(audioCtx.destination);
+
+// const dataArray = new Uint8Array(bufferLength);
+// analyser.getByteTimeDomainData(dataArray);
+
+
+// //D3 Setup
+// const x = d3.scaleLinear().
+// domain([0, analyser.frequencyBinCount]).
+// range([0, width]);
+
+// const y = d3.scaleLinear().
+// domain([-175, 175]).
+// range([height, 175]);
+
+// const line = d3.line().
+// x(function(d, i) {return x(i);}).
+// y(function(d) {return y(d);}).
+// context(canvasCtx);
+
+
+// function renderFullLine(d){
+//     canvasCtx.strokeStyle = "rgb(255, 0,  100)";
+//     canvasCtx.beginPath();
+//     line(d);
+//     canvasCtx.stroke();
+// } 
+
+// function renderLoop(){
+//     analyser.getByteTimeDomainData(dataArray);
+//     canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+//     waveData.next(dataArray);
+// }
+
+// waveData.subscribe(
+//     d => {
+//         return renderFullLine(d);
+//     }
+// )
+
+
+// const loop = interval(0, animationFrameScheduler).pipe(
+//     // take(2000)
+// ).subscribe(renderLoop);
 
 
 
